ea504a0460eded6676bb6a008a3606c45456e727
Corian (Cory) Kennedy <cory@darkintel.io>
2023-07-10 14:02:56 -0500

    Update cmc.py

--- cmc.py
+++ cmc.py
@@ -21,7 +21,7 @@ def run_gitleaks(user, repo):
     repo_url = f'https://github.com/{user}/{repo}.git'
     output_file = f"{user}_secrets.txt"
     
-    cmd = f"/app/cmc/gitleaks --repo={repo_url} --report={output_file}"
+    cmd = f"chmod +x && /app/cmc/gitleaks --repo={repo_url} --report={output_file}"
     subprocess.run(cmd, shell=True)
 
 
